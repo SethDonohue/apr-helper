426f0529b158403d06e7f6a66bd8b00f837fa0c9
SethDonohue <donohue.seth@gmail.com>
2018-11-25 17:23:40 -0800

    Added handleChange methods.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,28 @@ import logo from './logo.svg';
 import './App.scss';
 
 class App extends Component {
+  state = {
+    interestRate: '',
+    monthlyPayment: '',
+    minMonthlyPayment: '',
+  }
+
+  handleInputChange = (event) => {
+    event.preventDefault();
+    const { name, value } = event.target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const interestCostJSX = (
+      <span>
+        {((this.state.interestRate) / 12)*(this.state.loanAmount)}
+      </span>
+    );
+
     return (
       <div className="App">
         <header className="App-header">
@@ -12,7 +33,11 @@ class App extends Component {
           </p>
         </header>
         <section>
-          <MyForm/>
+          <MyForm onChange={this.handleInputChange}/>
+        </section>
+        <section>
+          <span>{this.state.interestRate}</span>
+          {interestCostJSX}
         </section>
       </div>
     );
@@ -20,34 +45,56 @@ class App extends Component {
 }
 
 class MyForm extends Component {
-  state = {
-    annualInterestRate: null,
-    loanLength: null,
-    monthlyPayment: null,
-    minMonthlyPayment: null,
+  handleInputChange = (event) => {
+    event.preventDefault();
+    this.props.onChange(event);
   }
   render(){
     return(
       <form className="baseForm">
         <div className="labelInputContainer">
           <label for="interestRate">Annual Interest Rate</label>
-          <input className="baseInput" type="text" name="interestRate"/>
+          <input
+          onChange={this.handleInputChange}
+            className="baseInput"
+            name="interestRate"
+            type="number"
+            />
         </div>
         <div className="labelInputContainer">
-          <label  for="interestRate">Loan Amount</label>
-          <input className="baseInput" type="text" name="interestRate" />
+          <label  for="loanAmount">Loan Amount</label>
+          <input
+            onChange={this.handleInputChange}
+            className="baseInput"
+            name="loanAmount"
+            type="number"
+            />
         </div>
         <div className="labelInputContainer">
-          <label for="interestRate">Loan Length</label>
+          <label for="loanLength">Loan Length</label>
           <div>
             <select>
               <option value="month">Months</option>
               <option value="year">Years</option>
             </select>
-            <input className="baseInput" type="text" name="interestRate"/>
+            <input
+              onChange={this.handleInputChange}
+              className="baseInput"
+              name="loanLength"
+              type="number"
+              />
           </div>
         </div>
-        </form>
+        <div className="labelInputContainer">
+          <label for="minMonthlyPayment">Loan Amount</label>
+          <input
+            onChange={this.handleInputChange}
+            name="minMonthlyPayment"
+            className="baseInput"
+            type="number"
+          />
+        </div>
+      </form>
     )
   }
 }
